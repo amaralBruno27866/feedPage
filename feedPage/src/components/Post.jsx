892ec1a980feb1ed960dbbd0df54d872fa021c4d
amaralBruno27866 <bamaral2.git2023@gmail.com>
2023-08-24 12:02:18 -0400

    Fixing Post component

--- feedPage/src/components/Post.jsx
+++ feedPage/src/components/Post.jsx
@@ -1,6 +1,6 @@
-import styles from '../styles/Post.module.css'
 import { Avatar } from './Avatar';
 import { Comment } from './Comment';
+import styles from '../styles/Post.module.css'
 
 export function Post(){
     return(
@@ -8,28 +8,31 @@ export function Post(){
             <header>
                 <div className={styles.author}>
 
-                    <Avatar src="https://github.com/amaralBruno27866.png" alt="User picture" />
+                    <Avatar src="https://github.com/NyctibiusVII.png" alt="User picture" />
 
                     <div className={styles.authorInfo}>
-                        <strong>Bruno Amaral</strong>
-                        <span>Intership Programmer</span>
+                        <strong>NyctibiusVII</strong>
+                        <span>Front-End Programmer</span>
                     </div>
+                </div>
 
-                    <time title="date and hour" dateTime="date USA">
+                    <time title="May 13 at 08:13PM" dateTime="2023-05-13 20:13:30">
                         Published 1 hour ago
                     </time>
 
-                    <div className={styles.content}>
-                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing 
-                            elit. Atque autem quasi maiores, odit exercitationem 
-                            at iste laborum soluta delectus quis! Molestiae 
-                            quibusdam numquam similique aliquam dolore dolorum 
-                            reprehenderit facere obcaecati.
-                        </p>
-                    </div>
-
-                </div>
             </header>
+
+            <div className={styles.content}>
+                    <p>Hey guys 👋</p>
+                    <p>I just uploaded another project to my portfolio. It's a project I did at NLW Return, a Rocketseat event. Project name is DoctorCare 🚀</p>
+                    <p>👉{''}<a>jane.design/doctorcare</a></p>
+                    <p>
+                        <a>#newproject </a>{''}
+                        <a>#nlw </a>{''}
+                        <a>#rocketseat</a>
+                    </p>
+            </div>
+
             <form className={styles.commentForm}>
                 <strong>Leave your comment</strong>
                 <textarea placeholder="Write a comment"></textarea>
